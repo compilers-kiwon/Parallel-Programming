9883dc68d9cc656c14c11252f6032d4799cf5acb
Kiwon Kwon <kkwon@uei.com>
2023-07-05 09:35:45 +0900

    Optimization

--- CUDA/vector_add.cu
+++ CUDA/vector_add.cu
@@ -4,8 +4,8 @@
 
 #define MAX_SIZE    4000000
 
-#define NUM_OF_BLOCKS               400
-#define NUM_OF_THREADS_PER_BLOCK    1000
+#define NUM_OF_BLOCKS               200
+#define NUM_OF_THREADS_PER_BLOCK    20000
 #define VECTOR_SIZE_PER_THREAD      (MAX_SIZE/(NUM_OF_BLOCKS*NUM_OF_THREADS_PER_BLOCK))
 
 #define DST     0
@@ -13,14 +13,12 @@
 #define SRC2    2
 #define MAX_MEM 3
 
-#define mem
+#define min(a,b)    ((a)<(b)?(a):(b))
+
 __global__ void vector_add(int *dst,int *src1,int* src2)
 {
     int begin = (blockDim.x*blockIdx.x+threadIdx.x)*VECTOR_SIZE_PER_THREAD;
-    int end = begin+VECTOR_SIZE_PER_THREAD;
-
-    //printf("(%d,%d) thread takes vector[%d~%d]\n",
-    //                    blockIdx.x,threadIdx.x,begin,end);
+    int end = min(begin+VECTOR_SIZE_PER_THREAD,MAX_SIZE);
 
     for(;begin<end;begin++)
     {
@@ -55,24 +53,7 @@ int main(void)
     vector_add<<<NUM_OF_BLOCKS,NUM_OF_THREADS_PER_BLOCK>>>(cuda_mem[DST],cuda_mem[SRC1],cuda_mem[SRC2]);
     cudaDeviceSynchronize();
     cudaMemcpy(cpu_mem[DST],cuda_mem[DST],sizeof(int)*MAX_SIZE,cudaMemcpyDeviceToHost);
-/*
-    for(i=0;i<MAX_SIZE;i++)
-    {
-        if( cpu_mem[SRC1][i]+cpu_mem[SRC2][i] != cpu_mem[DST][i] )
-        {
-            printf("Unexpected!!\n");
-            printf("[%07d] %d = %d + %d\n",i,cpu_mem[DST][i],cpu_mem[SRC1][i],cpu_mem[SRC2][i]);
-            break;
-        }
-        else
-        {
-            if( i%1000000 == 0 )
-            {
-                printf("[%07d] %d = %d + %d\n",i,cpu_mem[DST][i],cpu_mem[SRC1][i],cpu_mem[SRC2][i]);
-            }
-        }
-    }
-*/
+
     for(i=0;i<MAX_MEM;i++)
     {
         free(cpu_mem[i]);
