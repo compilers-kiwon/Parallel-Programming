744209e0e195874d026689f554dfa35e28827e84
compilers-kiwon <compilers@naver.com>
2023-08-14 16:42:04 +0900

    Multiple attributes for one function

--- CUDA/hello.cu
+++ CUDA/hello.cu
@@ -1,26 +1,20 @@
 #include <stdio.h>
 
-__global__ void helloFromHost();
-__device__ int helloFromDevice(int tid);
+__host__ __device__ void Print()
+{
+    printf("Hello World\n");
+}
+
+__global__ void Wrapper()
+{
+    Print();
+}
 
 int main()
 {
-    helloFromHost<<<1,10>>>();
+    Print();
+    printf("==================\n");
+    Wrapper<<<1,5>>>();
     cudaDeviceReset();
     return 0;
-}
-
-__global__ void helloFromHost()
-{
-    int tid = threadIdx.x;
-    printf("Hello world From __global__ kernel: %d\n", tid);
-    
-    int tid1 = helloFromDevice(tid);
-    printf("tid1 : %d\n", tid1);
-}
-
-__device__ int helloFromDevice(int tid)
-{
-    printf("Hello world Form __device__ kernel: %d\n",tid);
-    return tid+1;
 }
